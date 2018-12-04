1e6a00b633611398e859abe5a62fae333449c1e1
janglee123 <merupatel123@gmail.com>
2018-12-04 22:57:21 +0530

    open file added

---
+++ README.md
@@ -0,0 +1,4 @@
+# Eplee
+- based on [Epub.js Reader](https://github.com/futurepress/epubjs-reader)
+- early age of devlopment
+ 
--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,50 @@
 const electron = require('electron');
+const electronLocalshortcut = require('electron-localshortcut');
 const path  = require('path');
+const url = require('url')
 
-/*const {
-    app,
-    BrowserWindow,
-    Menu,
-    ipcMain,
-    dialog,
-    powerSaveBlocker
-} = electron;*/
-
-const{app,BrowserWindow,icpMain} = electron
-
+const{app, BrowserWindow, dialog, Menu, MenuItem} = electron;
+menu = new Menu();
 let win;
 
 function createWindow(){
     win = new BrowserWindow({
         width:960,
         height:640,
-        //kiosk:true,
         //frame: false,
         webSecurity: false,
-        //titleBarStyle: 'hiddenInset',
-        //backgroundColor: '#2e2c29',
         useContentSize: true
     });
-    //win.setMenu(null)
-    win.loadFile(path.join(__dirname,'..','html','reader.html'));
+
+    electronLocalshortcut.register('Ctrl+O', () => {
+        console.log('openDialog open');
+        let pathtoepub = dialog.showOpenDialog(
+            {
+                filters:[{ name: 'ePub', extensions: 'epub' }],
+                properties:['openFile'],
+            }
+        )[0];
+        console.log(pathtoepub);
+
+        if(!pathtoepub){
+            return;
+        }
+        if(!pathtoepub.endsWith('.epub')){
+            dialog.showMessageBox({message:'Please select ePub file'});
+            return;
+        }
+
+        win.loadURL(url.format({
+            slashes: true,
+            protocol: 'file:',
+            pathname: path.join(__dirname,'..','html','reader.html'),
+            query: {
+                bookPath:pathtoepub
+            }
+        }));
+
+    });
+    win.loadFile(path.join(__dirname,'..','html','library.html'));
 }
 
 app.on('ready',createWindow)
