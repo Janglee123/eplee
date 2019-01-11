c93890cf068e1e1094150e5c06807fdf06650d52
Your Name <you@example.com>
2019-01-12 01:19:49 +0530

    some fix

--- src/js/main.js
+++ src/js/main.js
@@ -12,37 +12,48 @@ function createWindow(){
         width:1042,
         height:640,
         frame: false,
+        devtools: true,
         webSecurity: false,
         useContentSize: true,
         transparent: true,
+        webPreferences: {
+            experimentalFeatures: true
+        },
         icon: path.join(__dirname,'..','img','icons','64x64.png')
     });
 
     electronLocalshortcut.register(win, 'Ctrl+O', () => {
-        console.log('openDialog open');
-        let pathtoepub = dialog.showOpenDialog(
+        
+        let file = dialog.showOpenDialog(
             {
                 filters:[{ name: 'ePub', extensions: ['epub'] }],
                 properties:['openFile'],
             }
         );
-        if(!pathtoepub){
+        if(!file){
             return;
         }
-        pathtoepub = pathtoepub[0];
+        
+        Library.addBook(file);
 
         win.loadURL(url.format({
             slashes: true,
             protocol: 'file:',
             pathname: path.join(__dirname,'..','html','reader.html'),
             query: {
-                bookPath:pathtoepub
+                bookPath:file
             }
         }));
-
     });
+
+    electronLocalshortcut.register(win, 'Ctrl+I', () =>{
+        win.webContents.toggleDevTools()
+    });
+
+
+
     win.loadFile(path.join(__dirname,'..','html','library.html'));
 }
 
-app.on('ready',() => setTimeout(createWindow, 100));
-app.disableHardwareAcceleration()
+app.on('ready',() => setTimeout(createWindow,100));
+app.disableHardwareAcceleration();
