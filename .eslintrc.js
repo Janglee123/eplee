a0739b13a521dcf93edf9ab9d600a86ed8332a0f
janglee123 <merupatel123@gmail.com>
2019-05-07 01:54:38 +0530

    eslint rules updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,6 @@ module.exports = {
     semi: 0,
     'comma-dangle': 0,
     'no-console': 0,
-    'arrow-parens':1,
     'global-require': 0,
     'import/no-unresolved': 0,
     'no-param-reassign': 0,
